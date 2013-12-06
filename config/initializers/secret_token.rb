ba626e22a144f51f9ba05e82de7f66b0b4cda820
Antonio <mtxcm22@gmail.com>
2013-12-06 10:05:38 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeezeMs::Application.config.secret_key_base = '4494a51a6af7470d8cee9d946d49d9e212576c8b3e066e7a4e78a3fad9ed90c08d25a8801bc8e300c0f7b1600f9df84aa5e5b9e851fbceb2beffdc038c522c5f'
